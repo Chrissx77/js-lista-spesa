88a79b353df6611bb0d5e3294c93fe28a99e9022
ChrisGarozzo <garozzochristian@gmail.com>
2023-11-24 14:36:01 +0100

    added logic js with while & insert food button

--- js/main.js
+++ js/main.js
@@ -1,15 +1,30 @@
-const arrayFood = ["patate","carote","pasta","pane","pesto"];
+const arrayFood = ["patate", "carote", "pasta", "pane", "pesto"];
 const listUl = document.querySelector(".foodList");
-
-
+const userInsert = document.querySelector(".food");
+const btnInsert = document.getElementById("btn");
 
 
 let i = 0;
-while(i < arrayFood.length){
+while (i < arrayFood.length) {
     thisFood = arrayFood[i];
     const elementLi = document.createElement("li");
     elementLi.append(thisFood);
     listUl.append(elementLi);
     i++;
-    // console.log(arrayFood[i]);
 }
+
+btnInsert.addEventListener("click",
+    function () {
+        let food = userInsert.value;
+        arrayFood.push(food);
+        let i = 0;
+        while (i < arrayFood.length) {
+            thisFood = arrayFood[i];
+            const elementLi = document.createElement("li");
+            elementLi.append(thisFood);
+            listUl.append(elementLi);
+            i++;
+        }
+    }
+)
+
